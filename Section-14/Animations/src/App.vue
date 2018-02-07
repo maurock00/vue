87825ffbf617ba4216af0600271ce2414cbb5757
maurock00 <izardui00@gmail.com>
2018-02-06 23:03:25 -0500

    added Section-14 Animations and basic animation to render elements with v-if or v-show

---
+++ Section-14/Animations/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-primary" @click=" show = !show"> Show an alert ! </button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show"> This is some info </div>
+                </transition>
+                 <transition name="slide" type="animation">
+                    <div class="alert alert-info" v-if="show"> This is some info </div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+
+.fade-enter { 
+    opacity: 0;
+}
+
+.fade-enter-active {
+    transition: opacity 1s;
+}
+
+.fade-leave { 
+    /*opacity: 1; */
+}
+
+.fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+.slide-enter { 
+    /* transform: translateX(-20px); */
+    opacity: 0;
+}
+
+.slide-enter-active { 
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+}
+
+.slide-leave { 
+    
+}
+
+
+.slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
+}
+
+@keyframes slide-in {
+    from { 
+        transform: translateX(-20px); 
+    }
+    to { 
+        transform: translateX(0);
+    }
+}
+
+@keyframes slide-out {
+    from { 
+        transform: translateX(0px); 
+    }
+    to { 
+        transform: translateX(-20px);
+    }
+}
+
+</style>
