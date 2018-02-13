e371fb973c157199b049cd37521da1cf8d42dccb
maurock00 <izardui00@gmail.com>
2018-02-13 16:42:53 -0500

    Added Section 16 Router

---
+++ Section-16/Router/src/components/user/User.vue
@@ -0,0 +1,17 @@
+<template>
+    <div> 
+        <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary" @click="backToHome"> Back Home </button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: { 
+      backToHome() { 
+          this.$router.push('/');
+      }
+  }
+}
+</script>
