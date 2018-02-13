6703a8ace58804bbc712e310f112a0f752683bcc
maurock00 <izardui00@gmail.com>
2018-02-12 21:28:18 -0500

    HTTP on vue.js with vue-resources

--- Section-15/HTTPS/src/App.vue
+++ Section-15/HTTPS/src/App.vue
@@ -12,6 +12,16 @@
                     <input type="text" class="form-control" v-model="user.userEmail">
                 </div>
                 <button class="btn btn-primary" @click="save"> Save ! </button>
+                <hr>
+                <h2> fectching data </h2>
+                <input type="text" class="form-control" v-model="node">
+                <button class="btn btn-primary" @click="fetchData"> Get Data </button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="(u, index) in users" :key="index">
+                       {{ u.username }}  ----  {{ u.userEmail }}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,18 +34,54 @@
                 user: { 
                     username: '', 
                     userEmail: ''
-                }
+                }, 
+                users: [],
+                resource: {}, 
+                node: 'user'
             }
         }, 
         methods: { 
             save(){ 
-                this.$http.post('https://vuejs-http-e0872.firebaseio.com/user.json', this.user)
-                        .then(response => { 
-                            console.log(response);
-                        }, error => { 
-                            console.log(error); 
+                // this.$http.post('user.json', this.user)
+                //         .then(response => { 
+                //             console.log(response);
+                //         }, error => { 
+                //             console.log(error); 
+                //         });
+                //this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user); 
+            }, 
+            fetchData() { 
+                /*this.$http.get('user.json')
+                        .then( response => { 
+                            return response.json();
+                        })
+                        .then(data => { 
+                            //this.users = data; first method 
+                            //Second Method
+                            const resultArray = []; 
+                            for( let key in data)
+                                resultArray.push(data[key]); 
+                            this.users = resultArray;
+                        });*/
+                this.resource.getData({node: this.node} )
+                        .then( response => { 
+                            return response.json();
+                        })
+                        .then(data => { 
+                            const resultArray = []; 
+                            for( let key in data)
+                                resultArray.push(data[key]); 
+                            this.users = resultArray;
                         });
             }
+        }, 
+        created() {
+            const customActions = { 
+                saveAlt: { method: 'POST', url: 'alternative.json' }, 
+                getData: { method: 'GET'}
+            }; 
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
--- Section-15/HTTPS/src/main.js
+++ Section-15/HTTPS/src/main.js
@@ -4,6 +4,21 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-e0872.firebaseio.com/';
+
+//Interceptors to execute code between requests ... to interecept responses it coul be done on next(); function
+//PUT method overwrites the data on FIREBASE, is the behavior on FIREBASE
+
+Vue.http.interceptors.push((request, next) => { 
+  console.log(request); 
+  if(request.method == "POST")
+    request.method = "PUT";
+  
+  next(response => { 
+    response.json = () => { return { messages: response.body } }
+  });
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
